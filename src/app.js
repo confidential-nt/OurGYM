97de54ac7cc0bdf5dd294bb1eaf0e9fdfe5d712e
confidential-nt <youyoy689@gmail.com>
2021-09-30 21:11:55 +0900

    회원가입, 로그인 pug 수정/ morgan 패키지 추가

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
+const logger = morgan("dev");
+
 app.set("views", process.cwd() + "/src/views");
 app.set("view engine", "pug");
 
+app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", globalRouter);
