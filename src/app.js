bb16a08dba2bca9231e70b20af5a4628083583a7
confidential-nt <youyoy689@gmail.com>
2021-10-05 20:37:55 +0900

    .env 변수로 중요정보 대체

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     resave: true,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/ourgym" }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
