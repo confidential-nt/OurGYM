d68aef831330a2c872cf12203923834a5eee0a35
confidential-nt <youyoy689@gmail.com>
2021-11-23 18:15:01 +0900

    프로필 css 수정

--- src/client/js/home.js
+++ src/client/js/home.js
@@ -1,73 +1,73 @@
-import User from "../models/User";
+// import User from "../models/User";
 
-const id = req.session.user._id;
-const user = await User.findById(id);
+// const id = req.session.user._id;
+// // const user = await User.findById(id);
 
-const updateExrtime = async (req, res)=>{
-  await User.findByIdAndUpdate(_id, { exercises: { exrtime } } );
-}
+// const updateExrtime = async (req, res) => {
+//   await User.findByIdAndUpdate(_id, { exercises: { exrtime } });
+// };
 
-//Paint current date
-const currentDate = document.querySelector(".content_top");
+// //Paint current date
+// const currentDate = document.querySelector(".content_top");
 
-function paintCurrentDate() {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const dateToday = date.getDate();
-  currentDate.innerText = `${String(year)}. ${String(month).padStart(
-    2,
-    "0"
-  )}. ${String(dateToday).padStart(2, "0")}`;
-}
-paintCurrentDate();
+// function paintCurrentDate() {
+//   const date = new Date();
+//   const year = date.getFullYear();
+//   const month = date.getMonth() + 1;
+//   const dateToday = date.getDate();
+//   currentDate.innerText = `${String(year)}. ${String(month).padStart(
+//     2,
+//     "0"
+//   )}. ${String(dateToday).padStart(2, "0")}`;
+// }
+// paintCurrentDate();
 
-//Timer
-const exr_timer = document.getElementsByClassName("exr_timer");
-//timer 클릭되면 index 추출해서 exr_timer_index + time 만듦
-let index = 1;
+// //Timer
+// const exr_timer = document.getElementsByClassName("exr_timer");
+// //timer 클릭되면 index 추출해서 exr_timer_index + time 만듦
+// let index = 1;
 
-// change start & pause
-function changeValues(event) {
-  const exr_timer_index = document.getElementById(`exr_timer_${index}`);
-  let timerInterval;
+// // change start & pause
+// function changeValues(event) {
+//   const exr_timer_index = document.getElementById(`exr_timer_${index}`);
+//   let timerInterval;
 
-  if (exr_timer_index.textContent === "▶") {
-    exr_timer_index.innerHTML = "| |";
-    const date = new Date();
-    const clickedTime = date.getTime();
-    timerInterval = setInterval(function () {
-      changeTime(clickedTime);
-    }, 1000);
-  } else {
-    //update mongo
-    exr_timer_index.innerHTML = "▶";
-    clearInterval(timerInterval);
-  }
-}
+//   if (exr_timer_index.textContent === "▶") {
+//     exr_timer_index.innerHTML = "| |";
+//     const date = new Date();
+//     const clickedTime = date.getTime();
+//     timerInterval = setInterval(function () {
+//       changeTime(clickedTime);
+//     }, 1000);
+//   } else {
+//     //update mongo
+//     exr_timer_index.innerHTML = "▶";
+//     clearInterval(timerInterval);
+//   }
+// }
 
-//change value of time
-function changeTime(clickedTime) {
-  const exr_timer_time = document.getElementById(`exr_timer_time_${index}`);
-  const date = new Date();
-  const sumOfTime = parseInt((date.getTime() - clickedTime) / 1000);
-  updateExrtime();
-  console.log(sumOfTime);
-  console.log(typeof sumOfTime);
-  const hours = parseInt(sumOfTime / 3600);
-  const minutes = parseInt((sumOfTime % 3600) / 60);
-  const seconds = parseInt((sumOfTime % 3600) % 60);
+// //change value of time
+// function changeTime(clickedTime) {
+//   const exr_timer_time = document.getElementById(`exr_timer_time_${index}`);
+//   const date = new Date();
+//   const sumOfTime = parseInt((date.getTime() - clickedTime) / 1000);
+//   updateExrtime();
+//   console.log(sumOfTime);
+//   console.log(typeof sumOfTime);
+//   const hours = parseInt(sumOfTime / 3600);
+//   const minutes = parseInt((sumOfTime % 3600) / 60);
+//   const seconds = parseInt((sumOfTime % 3600) % 60);
 
-  exr_timer_time.innerText = `${String(hours).padStart(2, "0")} : ${String(
-    minutes
-  ).padStart(2, "0")} : ${String(seconds).padStart(2, "0")}`;
-}
+//   exr_timer_time.innerText = `${String(hours).padStart(2, "0")} : ${String(
+//     minutes
+//   ).padStart(2, "0")} : ${String(seconds).padStart(2, "0")}`;
+// }
 
-for (var i = 0; i < exr_timer.length; i++) {
-  (function (idx) {
-    exr_timer[idx].onclick = function () {
-      index = idx + 1;
-      changeValues();
-    };
-  })(i);
-}
+// for (var i = 0; i < exr_timer.length; i++) {
+//   (function (idx) {
+//     exr_timer[idx].onclick = function () {
+//       index = idx + 1;
+//       changeValues();
+//     };
+//   })(i);
+// }
--- src/models/User.js
+++ src/models/User.js
@@ -6,16 +6,16 @@ const objectId = mongoose.Schema.Types.ObjectId;
 const userSchema = mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  profileImg: { type: String },
+  profileImg: { type: String, default: "" },
   nickname: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   mainSports: { type: String, required: true },
-  exercises: [
-    {
-      exrname: { type: String },
-      exrtime: { type: Number},
-    },
-  ],
+  // exercises: [
+  //   {
+  //     exrname: { type: String },
+  //     exrtime: { type: Number },
+  //   },
+  // ],
   gender: { type: String },
   birthday: { type: String },
   //like this?
