7ddbd9608b4747e66de54c483df0b6a90d937ced
confidential-nt <youyoy689@gmail.com>
2021-11-30 20:53:56 +0900

    랭킹 완료 및 캘린더 일부 완료

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { getDailyLogInfo } from "../controllers/dailyLogController";
+import { getExerciseInfo } from "../controllers/exerciseController";
 import { addTime, deleteExr } from "../Controllers/globalController";
 
 const apiRouter = express.Router();
@@ -8,6 +9,6 @@ const apiRouter = express.Router();
 apiRouter.get("/daily-log/:id", getDailyLogInfo);
 apiRouter.post("/timer/time", addTime);
 apiRouter.post("/timer/time/remove", deleteExr);
-apiRouter.get("/exercise/data");
+apiRouter.get("/exercise/data", getExerciseInfo);
 
 export default apiRouter;
